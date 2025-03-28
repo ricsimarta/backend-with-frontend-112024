f13e660d85152f1ecf151a7929ae02357cb47502
ricsimarta <ricsimarta@gmail.com>
2025-03-28 19:58:37 +0100

    added addtocart, and delete events

--- backend/index.js
+++ backend/index.js
@@ -18,48 +18,25 @@ app.get('/api/data', (req, res) => {
 });
 
 app.post('/api/data/new', (req, res) => {
-  console.log(req.body);
-  const { id, name, price, rating } = req.body;
-
-  /* const objKeys = Object.keys(req.body);
-
-  if (objKeys.length < 4) return res.status(406).send('incomplete data');
-
-  for (let i = 0; i < objKeys.length; i++) {
-    if (!req.body[objKeys[i]]) return res.status(406).send('incomplete data');
-  }
-
-  if (!req.body.id || !req.body.name || !req.body.price || !req.body.rating || typeof req.body.rating !== 'number' || typeof req.body.price !== 'number') {
-    return res.status(406).send('incomplete data');
-  } */
-
-
   fs.readFile(`${__dirname}/data/beers.json`, 'utf8', (err, dataString) => {
     if (err) {
       console.log('error at reading file');
-      return res.status(500).send('error at reading file');
+      return res.status(500).json('error at reading file');
     }
 
     const data = JSON.parse(dataString);
-
     const newBeer = { ...req.body, id: data[data.length - 1].id + 1 }
-
-    /* const found = data.find(beer => beer.id === id);
-    if (found) return res.status(409).send('id is already in use'); */
-
     data.push(newBeer);
 
     fs.writeFile(`${__dirname}/data/beers.json`, JSON.stringify(data, null, 2), (err) => {
       if (err) {
         console.log('error at writing file');
-        return res.status(500).send('error at writing file');
+        return res.status(500).json('error at writing file');
       }
 
-      return res.send(newBeer);
+      return res.json(newBeer);
     })
   })
-
-  //res.send('ok');
 })
 
 app.listen(port, () => {
--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,12 +1,35 @@
-console.log('hello world');
+const rootElement = document.querySelector("#root");
+let beersData = [];
 
 const beerComponent = (beer) => `
   <div class="beer">
+    <button id="delete-${beer.id}" class="delete">x</button>
     <h2>${beer.name}</h2>
     <h3>${beer.price}</h3>
     <h4>${beer.rating}</h4>
+    <button id="add-${beer.id}" class="add">add to cart</button>
   </div>
 `;
+
+const findBeer = (id) => {
+  const beer = beersData.find(beerData => beerData.id === Number(id));
+  return beer;
+}
+
+const createAddBeerToCartEvents = () => {
+  document.querySelectorAll("button.add").forEach(button => button.addEventListener("click", (event) => {
+    const beerId = event.target.id.substring(4);
+    const beer = findBeer(beerId);
+  }))
+}
+
+const createDeleteBeerEvents = () => {
+  document.querySelectorAll("button.delete").forEach(button => button.addEventListener("click", () => {
+    const beerId = button.id.substring(7);
+    beersData = beersData.filter(beerData => beerData.id !== Number(beerId));
+    createDom();
+  }))
+}
 
 const beersComponent = (beers) => `
   <div class="beers">
@@ -14,9 +37,58 @@ const beersComponent = (beers) => `
   </div>
 `;
 
+const newBeerComponent = () => `
+  <form>
+    <input type="text" name="name" placeholder="beer name" />
+    <input type="number" name="price" placeholder="beer price" />
+    <input type="number" name="rating" placeholder="beer rating" />
+    <button>send</button>
+  </form>
+`;
+
+const createFormEvent = () => {
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const beerNameInput = document.querySelector('input[name="name"]');
+    const beerPriceInput = document.querySelector('input[name="price"]');
+    const beerRatingInput = document.querySelector('input[name="rating"]');
+
+    const newBeer = {
+      name: beerNameInput.value,
+      price: Number(beerPriceInput.value),
+      rating: Number(beerRatingInput.value)
+    }
+
+    fetch('/api/data/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newBeer)
+    })
+      .then(res => res.json())
+      .then(newData => {
+        beersData.push(newData);
+        createDom();
+      })
+  })
+}
+
+const createDom = () => {
+  rootElement.innerHTML = "";
+
+  rootElement.insertAdjacentHTML("beforeend", beersComponent(beersData));
+  createAddBeerToCartEvents();
+  createDeleteBeerEvents();
+
+  rootElement.insertAdjacentHTML("beforeend", newBeerComponent());
+  createFormEvent();
+}
+
 fetch('/api/data')
   .then(res => res.json())
   .then(data => {
-    const rootElement = document.querySelector("#root");
-    rootElement.insertAdjacentHTML("beforeend", beersComponent(data));
+    beersData = data;
+    createDom();
   });
